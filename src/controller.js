f1d361d8c1f4b134703cd0ccab9f92e20dc88ed0
BarBronshtein <barbronsthein@gmail.com>
2022-06-08 23:54:06 +0300

    updated features changing background color

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+onSetLoc();
+
 function changeColors() {
   const user = getUser();
   if (!user) return;
@@ -9,4 +11,38 @@ function changeColors() {
 
 function showAge(val) {
   document.querySelector('.span-age').textContent = val;
+}
+
+function onSetLoc() {
+  setTimeout(
+    () =>
+      document.querySelector('.gm-svpc').addEventListener('click', getCurPos),
+    3000
+  );
+}
+function onAddMarker(ev) {
+  const name = prompt('Enter Place Name:');
+  const mark = new google.maps.Marker({
+    map: getMap(),
+    position: ev.latLng,
+    lat: ev.latLng.lat(),
+    lng: ev.latLng.lng(),
+  });
+  addMarker(mark, name);
+  renderPlaces();
+}
+
+function renderPlaces() {
+  const places = getPlaces();
+  let strHTML = '';
+  places.forEach(
+    place =>
+      (strHTML += `<a onclick="moveTo({lat:${place.lat},lng:${place.lng}})">${place.name} <span onclick="onDeletePlace(place.id)" class="del-place">X</span></a>`)
+  );
+  document.querySelector('.places-container').innerHTML = strHTML;
+}
+
+function onDeletePlace(placeId) {
+  deletePlace(placeId);
+  renderPlaces();
 }
--- src/services/placeService.js
+++ src/services/placeService.js
@@ -1,14 +1,82 @@
 'use strict';
 
-const GOOGLE_MAP_KEY = 'AIzaSyAl-v0FWCCcT0o6UrjDE17w4NIVtAa9AAI';
+const GOOGLE_MAP_KEY = 'AIzaSyByW4arVVCXU_wA07K5a2jrwLUJOKX64J4';
+const PLACE_STORAGE_KEY = 'placesDB';
 
 let gMap;
+const place = {
+  id: 1,
+  lat: 0,
+  lng: 0,
+  name: 'my house',
+};
+const gPlaces = [];
+
+function getMap() {
+  return gMap;
+}
 
 function initMap() {
   gMap = new google.maps.Map(document.querySelector('.map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 29.5576694, lng: 34.951925 },
+    zoom: 14,
   });
+  gMap.addListener('click', onAddMarker);
 }
 
-window.initMap = initMap;
+function getCurPos() {
+  navigator.geolocation.getCurrentPosition(getUserLoc, handleLocationError);
+}
+
+function getUserLoc(location) {
+  const { latitude, longitude } = location.coords;
+  const position = new google.maps.LatLng(latitude, longitude);
+  gMap.setCenter(position);
+}
+
+function addMarker(mark, name) {
+  const place = {};
+  place.name = name;
+  place.lat = mark.lat;
+  place.lng = mark.lng;
+  gPlaces.push(place);
+  saveToStorage(PLACE_STORAGE_KEY, gPlaces);
+}
+
+function deletePlace(placeId) {
+  const idx = gPlaces.findIndex(place => place.id === placeId);
+  gPlaces.splice(idx, 1);
+}
+
+function moveTo(position) {
+  console.log(position);
+  gMap.panTo(position);
+}
+
+function getPlaces() {
+  return gPlaces;
+}
+
+//Attach click event handler to the marker.
+function handleLocationError(err) {
+  var locationError = document.querySelector('.map');
+
+  switch (err.code) {
+    case 0:
+      locationError.innerHTML =
+        'There was an error while retrieving your location: ' + error.message;
+      break;
+    case 1:
+      locationError.innerHTML =
+        "The user didn't allow this page to retrieve a location.";
+      break;
+    case 2:
+      locationError.innerHTML =
+        'The browser was unable to determine your location: ' + error.message;
+      break;
+    case 3:
+      locationError.innerHTML =
+        'The browser timed out before retrieving the location.';
+      break;
+  }
+}
